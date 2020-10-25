4ebd29a7820794b26b1f753cc63ab95b4775776c
arnautovd <arnautov.dmitry2015@yandex.ru>
2020-10-25 15:08:51 +0300

    new file

---
+++ hello.js
@@ -0,0 +1,39 @@
+const func = () => {
+    let myBlockOfWords = 'hello world'.split(' ');
+    if (myBlockOfWords.length > 1) {
+        return myBlockOfWords.map(word => word.length)
+    }
+    return null;
+}
+
+console.log(func());
+
+const admins = [
+    {
+        name: 'Dima',
+        age: 29,
+        job: 'sysadmin',
+        skills: ['python', 'js', 'ansible']
+    },
+    {
+        name: 'Stas',
+        age: 30,
+        job: 'coder',
+        skills: ['C++', 'C#', 'asterisk']
+    },
+    {
+        name: 'Mike',
+        age: 22,
+        job: 'coder',
+        skills: ['C++', 'C#', 'asterisk']
+    },
+
+]
+
+const new_list = admins.map(admin => admin.job.replace('sysadmin', 'coder'))
+for (admin of admins) {
+    for (l of new_list) {
+        admin.job = l;
+    }
+}
+console.log(admins)
